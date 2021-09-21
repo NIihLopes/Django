c67f405330135922d6ef1ad7cbbce19e847334c4
Nicholas Lopes dos Santos <nicholas.lopes.98@hotmail.com>
2021-09-20 22:31:34 -0300

    trabalhando com models e banco de dados

--- Meu_projeto/contas/admin.py
+++ Meu_projeto/contas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import categorias
 # Register your models here.
+
+admin.site.register(categorias)
+
+# maneira de testar o funcionamento do banco de dados
+
--- Meu_projeto/contas/models.py
+++ Meu_projeto/contas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class categorias(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+    
