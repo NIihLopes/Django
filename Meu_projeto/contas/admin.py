7e8966e8d325addbee0771ebaa25c2609a9cde3d
Nicholas Lopes dos Santos <nicholas.lopes.98@hotmail.com>
2021-09-22 22:27:23 -0300

    inserindo a classe transação com models

--- Meu_projeto/contas/admin.py
+++ Meu_projeto/contas/admin.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
-from .models import categorias
 # Register your models here.
 
+from django.contrib import admin
+from .models import categorias
+from .models import transacao
+
+
 admin.site.register(categorias)
+admin.site.register(transacao)
+
 
 # maneira de testar o funcionamento do banco de dados
 
--- Meu_projeto/contas/models.py
+++ Meu_projeto/contas/models.py
@@ -5,4 +5,21 @@ from django.db import models
 class categorias(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
+
+    
+    def __self__(self):
+        return self.nome
+
+class transacao(models.Model):
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(categorias, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = "transações"
+        
+    def __str__(self):
+        return self.descricao
     
