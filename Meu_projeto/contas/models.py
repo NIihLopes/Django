b8f5418025cc61108a76eac08b5effb442378348
Nicholas Lopes dos Santos <nicholas.lopes.98@hotmail.com>
2021-09-23 23:02:01 -0300

    inserindo novo template e listando transações

--- Meu_projeto/contas/models.py
+++ Meu_projeto/contas/models.py
@@ -5,9 +5,8 @@ from django.db import models
 class categorias(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
-
     
-    def __self__(self):
+    def __str__(self):
         return self.nome
 
 class transacao(models.Model):
--- Meu_projeto/contas/views.py
+++ Meu_projeto/contas/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-
 # Create your views here.
-
-from django.http import HttpResponse
+from .models import transacao
+from django.shortcuts import render
 import datetime
 
 
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['transacoes'] = ['ti','t2','t3']
+    data['now'] = datetime.datetime.now()
     #html = "<html><body>agora são %s.</body></html>" % now
-    return render(request,'contas/home.html')
+    return render(request,'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = transacao.objects.all()
+    return render(request,'contas/listagem.html', data)
